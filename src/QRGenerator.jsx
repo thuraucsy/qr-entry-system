79f1a368cb0c4134cb64bf414c6be744b0791423
Thura Aung <thura.ucsy@gmail.com>
2025-06-16 21:23:11 +0900

    added qr poster

--- src/QRGenerator.jsx
+++ src/QRGenerator.jsx
@@ -2,8 +2,14 @@ import QRCode from "react-qr-code";
 
 const QRGenerator = ({ value }) => {
   return (
-    <div className="qr-generator">
-      <QRCode id="my-svg" value={value} size={256} />
+    <div className="qr-generator" id="qr-generator">
+      <h2>&nbsp;</h2>
+      <h2>&nbsp;</h2>
+      <h2>&nbsp;</h2>
+      <QRCode id="my-svg" value={value} size={150} />
+      <p className="scan-to-check-in">Scan to check-in</p>
+      <p>Place: <em>Okinawa Naha Hall</em></p>
+      <p>Time: 2025-07-21 Monday 15:00 (check-in 14:30)</p>
     </div>
   );
 };
--- src/QrDownload.jsx
+++ src/QrDownload.jsx
@@ -6,6 +6,7 @@ import DownloadIcon from '@mui/icons-material/Download';
 import QRGenerator from "./QRGenerator.jsx";
 import Swal from "sweetalert2";
 import { fetchSheetData } from "./utils.js";
+import html2canvas from "html2canvas";
 
 const downloadQR = (event, setQrGeneratorValue) => {
     const rows = JSON.parse(event.currentTarget.getAttribute("data-param-rows"));
@@ -17,7 +18,7 @@ const downloadQR = (event, setQrGeneratorValue) => {
 
     if (rows[qrno]) {
         setQrGeneratorValue(rows[qrno][2]); // Assuming the QR code value is in the third column (index 2)
-        setTimeout(() => saveSvgAsImage(qrno + ".png"), 500);
+        setTimeout(() => saveSvgAsImage(qrno + ".png"), 1000);
 
         Swal.fire({
             text: "QR Code downloaded successfully",
@@ -32,28 +33,49 @@ const downloadQR = (event, setQrGeneratorValue) => {
 }
 
 function saveSvgAsImage(imgName = "svg-image.png") {
-    const svgElement = document.getElementById("my-svg");
-    const svgData = new XMLSerializer().serializeToString(svgElement);
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
-    const img = new Image();
+    // const div = document.getElementById("qr-generator");
+    // html2canvas(div).then((canvas) => {
+    //     const imgData = canvas.toDataURL("image/png");
+    //     const link = document.createElement("a");
+    //     link.href = imgData;
+    //     link.download = imgName;
+    //     link.click();
+    // });
 
-    img.onload = function () {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0);
-        const pngData = canvas.toDataURL("image/png");
+    html2canvas(document.getElementById("qr-generator"), {
+        scrollY: -window.scrollY
+    }).then((canvas) => {
+        // document.body.appendChild(canvas);
 
-        // Create a download link
+        const imgData = canvas.toDataURL("image/png");
         const link = document.createElement("a");
-        link.href = pngData;
+        link.href = imgData;
         link.download = imgName;
-        document.body.appendChild(link);
         link.click();
-        document.body.removeChild(link);
-    };
+    });
 
-    img.src = "data:image/svg+xml;base64," + btoa(svgData);
+    // const svgElement = document.getElementById("qr-generator");
+    // const svgData = new XMLSerializer().serializeToString(svgElement);
+    // const canvas = document.createElement("canvas");
+    // const ctx = canvas.getContext("2d");
+    // const img = new Image();
+
+    // img.onload = function () {
+    //     canvas.width = img.width;
+    //     canvas.height = img.height;
+    //     ctx.drawImage(img, 0, 0);
+    //     const pngData = canvas.toDataURL("image/png");
+
+    //     // Create a download link
+    //     const link = document.createElement("a");
+    //     link.href = pngData;
+    //     link.download = imgName;
+    //     document.body.appendChild(link);
+    //     link.click();
+    //     document.body.removeChild(link);
+    // };
+
+    // img.src = "data:image/svg+xml;base64," + btoa(svgData);
 }
 
 export default function QrDownload() {
